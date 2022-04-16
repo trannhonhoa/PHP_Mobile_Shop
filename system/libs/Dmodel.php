2da2bc1f02ef157dda223d2c86495e5d5be84520
Tran Hoa <trannhonhoa2001@gmail.com>
2022-04-16 10:09:33 +0700

    'changedb'

--- system/libs/Dmodel.php
+++ system/libs/Dmodel.php
@@ -3,9 +3,9 @@
         protected $db;
         public function __construct()
         {
-            $connect  = "mysql:dbname=my_blog; host=localhost";
-            $user = "root";
-            $password = "trannhonhoa";
+            $connect  = "mysql:dbname=db_a858b3_dtdd; host=mysql8001.site4now.net";
+            $user = "a858b3_dtdd";
+            $password = "sql2022Hoa";
             $this->db = new Database($connect, $user, $password);
         }
        
